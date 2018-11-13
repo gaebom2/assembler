e6e439a0ebcad8aaedd002dda39e9312d20c8e72
gaebom2 <qkrgusdk110@naver.com>
2018-11-13 22:42:36 +0900

    final

--- trans.c
+++ trans.c
@@ -2,11 +2,11 @@
 
 int instr_trans(char *op, char *args, char* mcode)
 {
-	char* token1;
-	char* token2;
+	int i=0;
 	char temp[256];
-	char* src;
-	char* dest;
+	strcpy(temp,args);
+	char* token1=strtok(temp,",");
+	char* token2=strtok(NULL,"\n");
 
 	// check syntax 
 	if(!is_valid(op, args)){
@@ -14,45 +14,37 @@ int instr_trans(char *op, char *args, char* mcode)
 		return 0;
 	}
 
-
-	strcpy(mcode, "AB CD EF");
+	//strcpy(mcode, "AB CD EF");
 
 	/********************************/
-	strcmp(temp, args);
-	token1=strtok(temp,",");
-	token2=strtok(NULL,"\n");
-
-	//reg to reg
-	if(token1[0]=='%'&&token2[0]=='%')
-		strcpy(mcode, "89");
-	//mem to reg
-	else if(token1[0]=='('&&token2[0]=='%')
-		strcpy(mcode, "8b");
-	//mem to reg(eax)
-	else if(token1[0]=='0'&&token2[0]=='%')
-		strcpy(mcode," a1");
-	//reg(eax) to mem
-	else if(token1[0]=='%'&&token2[0]=='0')
-		strcpy(mcode, "a3");
-	//imm to reg
-	else if(token1[0]=='$'&&token2[0]=='%')
-		if(strcmp(token2, "%eax")==0)
-			strcpy(mcode, "b8");
-		else if(strcmp(token2, "%ecx")==0)
-			strcpy(mcode, "b9");
-		else if(strcmp(token2, "%edx")==0)
-			strcpy(mcode, "ba");
-		else if(strcmp(token2, "%ebx")==0)
-			strcpy(mcode, "bb");
-		else if(strcmp(token2, "%esp")==0)
-			strcpy(mcode, "bc");
-		else if(strcmp(token2, "%ebp")==0)
-			strcpy(mcode, "bd");
-		else if(strcmp(token2, "%esi")==0)
-			strcpy(mcode, "be");
-		else if(strcmp(token2, "%edi")==0)
-			strcpy(mcode, "bf");
-
+	while(args[i]!=NULL) {
+		if(args[i]==',') {
+			//reg to reg
+			if(token1[0]=='%'&&token2[0]=='%')
+				strcpy(mcode, "89");
+			//mem to reg
+			else if(args[i-1]==')'&&args[i+1]=='%')
+				strcpy(mcode, "8b");
+			//mem to reg(eax)
+			else if(token1[0]=='0'&&token2[0]=='%')
+				strcpy(mcode," a1");
+			//reg(eax) to mem
+			else if(token1[0]=='%'&&token2[0]=='0')
+				strcpy(mcode, "a3");
+			//imm to reg
+			else if(token1[0]=='$'&&token2[0]=='%') {
+				if(args[i+3]=='a' )
+					strcpy(mcode, "b8");
+				else if(args[i+3]=='b')
+					strcpy(mcode, "bb");
+				else if(args[i+3]=='c')
+					strcpy(mcode, "b9");
+				else if(args[i+3]=='d')
+					strcpy(mcode, "ba");
+			}
+		}
+		i++;
+	}
 	/********************************/
 	
 	return 1;	
