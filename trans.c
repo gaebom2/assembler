ae7f7402433556ed2582bc906e8e112af34d2cad
gaebom2 <qkrgusdk110@naver.com>
2018-11-13 14:52:40 +0900

    edit

--- trans.c
+++ trans.c
@@ -2,6 +2,11 @@
 
 int instr_trans(char *op, char *args, char* mcode)
 {
+	char* token1;
+	char* token2;
+	char* src;
+	char* dest;
+
 	// check syntax 
 	if(!is_valid(op, args)){
 		printf("Error: %s %s is not valid\n", op, args);
@@ -13,11 +18,13 @@ int instr_trans(char *op, char *args, char* mcode)
 
 	/********************************/
 
-	/*    generate machine code     */
-
 	if(!strcmp(op,"mov")) {
 		return 0;
 	}
+
+	strcpy(args, args);
+	token1=strtok(args,",");
+	tokne2=strtok(NUll," ");
 
 	
 
